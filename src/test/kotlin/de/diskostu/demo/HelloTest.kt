b317e14d4e1dcf2d698c67cf7a56ca9b46ff974e
diskostu <denny.kluge@gmail.com>
2018-10-07 08:33:33 +0200

    mini test

--- src/test/kotlin/de/diskostu/demo/HelloTest.kt
+++ src/test/kotlin/de/diskostu/demo/HelloTest.kt
@@ -1,11 +1,14 @@
 package de.diskostu.demo
 
 import kotlin.test.Test
+import kotlin.test.assertNotEquals
 
 class HelloTest {
 
     @Test
     fun foo() {
-
+        val a = "lala"
+        val b = "LALA"
+        assertNotEquals(a, b)
     }
 }
